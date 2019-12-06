f2e29049549c9ceef229f8eaa0838fe7495b607e
Vasyl Onopriienko <vasyl.o_ext@tandemploy.com>
2019-12-06 12:26:33 +0200

    task 3 done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'my-first-app';
+  isSecret = true;
+  logs = [];
+
+  displaySecret() {
+    this.isSecret = !this.isSecret;
+    this.logs.push(`${new Date().toDateString()}`);
+  }
+
+  getColor() {
+    return this.logs.length > 5 ? 'blue' : 'transparent';
+  }
 }
